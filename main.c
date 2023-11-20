ed2767d16a36deee9a9d0357cf85a41cb4a0532b
imtiaz4616 <imtiazuddinedu@gmail.com>
2023-11-21 02:15:54 +0600

    update

--- main.c
+++ main.c
@@ -2,123 +2,125 @@
 int main()
 {
     int i;
-    int ans1,ans2,ans3,ans4,ans5;
-    int point1, point2, point3, point4, point5, point6, point7, point8, point9, point10;
-    int point01, point02, point03, point04, point05, point06, point07, point08, point09, point010;
-    int total1,total2,total3;
+    int ans[5];
+    int point;
+    int point0;
+    int total1;
 
     printf("Welcome to the game\n");
-    printf(">Press 7 to start the game\n");
+    printf(">Press 1 to start the game\n");
     printf(">Press 0 to quit the game\n");
     scanf("%d",&i);
-    if(i==7){
-        printf("The game has Started\n\n")
-;    }
+    if(i==1){
+        printf("The game has Started\n\n");
+         }
     else if(i==0){
         printf("The game has ended\n\n");
     }
     else{
         printf("Invalid\n\n");
     }
-    if(i==7){
-        printf("1) Which one is  the first search engine in the internet\n\n ");
-        printf("1) Google\n");
-        printf("2) Archies\n");
-        printf("3) Wais\n");
-        printf("4) Altavista\n");
-        printf("Enter your answer: ");
-        scanf("%d",&ans1);
-        if(ans1==2){
+    if(i==1){
+        printf("1) What is the capital of Australia?\n\n");
+        printf("1) Sydney\n");
+        printf("2) Melbourne\n");
+        printf("3) Canberra\n");
+        printf("4) Brisbane\n\n\n");
+        printf("Enter your answer: \n");
+        scanf("%d",&ans[0]);
+        if(ans[0]==3){
             printf("Correct Answer\n");
-            point1=5;
-            printf("you have scored %d points\n",point1);
+            point=5;
+            printf("you have scored %d points\n\n",point);
             }
             else{
                 printf("Wrong Answer\n");
-                point01=0;
-                printf("you have scored %d points \n",point01);
+                point0=0;
+                printf("you have scored %d points \n\n\n",point0);
+            }
 
-            printf("2) Which one is the first web browser invented in 1990\n\n");
-            printf("1) Internet explorer\n");
-            printf("2) Mosaic\n");
-            printf("3) Mozilla\n");
-            printf("4) Nexus\n");
+            printf("2) Who wrote the play \"Romeo and Juliet\"?\n\n");
+            printf("1) Charles Dickens\n");
+            printf("2) William Shakespeare\n");
+            printf("3) Jane Austen\n");
+            printf("4) Leo Tolstoy\n\n\n");
 
-            printf("Enter your answer: ");
-            scanf("%d ",&ans2);
-            if(ans2==4){
+            printf("Enter your answer: \n");
+            scanf("%d",&ans[1]);
+            if(ans[1]==2){
                 printf("Correct answer\n");
-                point2=5;
-                printf("you have scored %d points \n",point2);
+                point=point+5;
+                printf("you have scored %d points\n\n\n",point);
                 }
             else{
                 printf("Wrong Answer\n");
-                point02=0;
-                printf("You have scored %d points\n",point02);
+                point0=0;
+                printf("You have scored %d points\n\n\n",point0);
             }
-            printf("3) First computer virus is known as?: \n\n");
-            printf("1) Rabbit\n ");
-            printf("2) Creeper virus \n");
-            printf("3) Elk cloner\n");
-            printf("4) SCA virus\n");
+            printf("3) In what year did the Titanic sink?\n\n");
+            printf("1) 1912\n");
+            printf("2) 1905\n");
+            printf("3) 1921\n");
+            printf("4) 1898\n\n\n");
 
-        printf("Enter your answer: \n");
-        scanf("%d",&ans3);
+        printf("Enter your answer:\n");
+        scanf("%d",&ans[2]);
 
-        if(ans3==2){
+        if(ans[2]==1){
             printf("Correct Answer\n");
-            point3=5;
-            printf("You have scored %d points",point3);
+            point=point+5;
+            printf("You have scored %d points\n\n\n",point);
         }
         else{
             printf("Wrong Answer\n");
-            point03=0;
-            printf("you have scored %d point/n",point03);
+            point0=0;
+            printf("you have scored %d point\n\n\n",point0);
         }
-            printf("Firewall in computer is used for?\n\n");
-            printf("1) Security\n ");
-            printf("2) Data transmission\n");
-            printf("3) Monitoring\n");
-            printf("4) Authentications\n");
+            printf("4) Which planet is known as the \"Red Planet\"?\n\n");
+            printf("1) Venus\n");
+            printf("2) Mars\n");
+            printf("3) Jupiter\n");
+            printf("4) Saturn\n\n\n");
 
             printf("Enter your answer:\n");
-            scanf("%d",&ans4);
+            scanf("%d",&ans[3]);
 
-            if(ans4==1){
+            if(ans[3]==2){
                 printf("Correct Answer\n");
-                point4=5;
-                printf("You have scored %d points\n");
+                point=point+5;
+                printf("You have scored %d points\n\n\n",point);
             }
             else{
                 printf("Wrong Answer\n");
-                point04=0;
-                printf("you have scored %d points\n",point04);
+                point0=0;
+                printf("you have scored %d points\n\n\n",point0);
             }
 
-            printf("5) Which of the following is not a database management software\n\n");
-            printf("Firewall in computer is used for?\n\n");
-            printf("1) Mysql\n ");
-            printf("2) Oracle \n");
-            printf("3) Cobal\n");
-            printf("4) Sybase\n");
+            printf("5) What is the largest mammal in the world?\n\n");
+            printf("1) African Elephant\n");
+            printf("2) Blue Whale\n");
+            printf("3) Giraffe\n");
+            printf("4) Polar Bear\n\n\n");
 
             printf("Enter you answer:\n");
-            scanf("%d",&ans5);
+            scanf("%d",&ans[4]);
 
-        if(ans5==0){
+        if(ans[4]==2){
             printf("Correct Answer\n");
-            point5=5;
-            printf("you have scored %d points",point5);
+            point=point+5;
+
+            printf("you have scored %d points\n\n\n",point);
         }
         else{
             printf("Wrong Answer\n");
-            point05=0;
-            printf("you have scored %d points",point05);
+            point0=0;
+            printf("you have scored %d points\n\n\n",point0);
         }
 
         }
+         return 0;
     }
 
 
-    return 0;
-}
+
+
